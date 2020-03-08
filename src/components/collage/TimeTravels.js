a0479adcc1bf251c5c23da2dc74b523aba21cf85
Qiao Lin <qiaoclin@gmail.com>
2020-03-08 23:40:59 +0100

    time travels animations

--- src/components/collage/TimeTravels.js
+++ src/components/collage/TimeTravels.js
@@ -5,6 +5,10 @@ import { remove } from "lodash"
 import { ParallaxLayer } from "react-spring/renderprops-addons.cjs"
 
 export default function({ layers }) {
+  const crystal = remove(layers, layer => layer.Key.includes("ANIMATION"))
+  const topLayer = remove(layers, layer => layer.Key.includes("02_TT"))
+
+  console.log(layers, crystal)
   return (
     <section id="TT-canvas">
       {layers.map((layer, index) => (
@@ -22,6 +26,27 @@ export default function({ layers }) {
           />
         </ParallaxLayer>
       ))}
+      <ParallaxLayer offset={4} speed={0}>
+        <Img
+          className="crystal"
+          fluid={crystal[0].childImageSharp.fluid}
+          objectPosition="0% 0%"
+          alt={`Time Travels crystal`}
+        />
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={4}
+        speed={4}
+        className="canvas-layer"
+        key="TT-top-layer"
+      >
+        <Img
+          fluid={topLayer[0].childImageSharp.fluid}
+          objectPosition="0% 0%"
+          alt="Time Travels"
+          className="collage-layer"
+        />
+      </ParallaxLayer>
     </section>
   )
 }
