dbff1bc5087452c45eec71131c1d21816b06e1c9
Qiao Lin <qiaoclin@gmail.com>
2020-03-09 16:50:17 +0100

    new gif for heart

--- src/components/collage/Existence.js
+++ src/components/collage/Existence.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Img from "gatsby-image"
 import { remove } from "lodash"
-import germanHeartGIF from "./exGermanHeart.gif"
+import germanHeartGIF from "./WIPHeart_GIF_4.gif"
 import { ParallaxLayer } from "react-spring/renderprops-addons.cjs"
 
 export default function({ layers }) {
