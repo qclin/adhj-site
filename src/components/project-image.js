b7d32333099fcb831b80d90e5998605cc9038f0e
qclin <qiaoclin@gmail.com>
2023-10-04 18:20:20 +0200

    fix aspect ratio check

--- src/components/project-image.js
+++ src/components/project-image.js
@@ -1,17 +1,16 @@
 import React from "react"
 import { GatsbyImage } from "gatsby-plugin-image";
+import clsx from 'clsx'; 
 
-export default function ProjectImage({ image, isResearch = false }) {
+const  ProjectImage = ({ image, isResearch = false }) => {
   const wrapperStyle = isResearch
     ? "w-25-ns pa2 dib image-figure"  
     : "w-60-ns margin-auto image-figure"
-
+  
   return (
     <figure
       className={
-        image.childImageSharp.gatsbyImageData.aspectRatio > 1
-          ? "landscapes " + wrapperStyle
-          : "portrait " + wrapperStyle
+        clsx(image.childImageSharp.gatsbyImageData.height > 1 && "portrait", wrapperStyle)
       }
     >
       <GatsbyImage image={image.childImageSharp.gatsbyImageData}
@@ -20,3 +19,5 @@ export default function ProjectImage({ image, isResearch = false }) {
     </figure>
   );
 }
+
+export default ProjectImage
